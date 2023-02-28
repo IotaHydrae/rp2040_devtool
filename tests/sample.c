9b5a4f83669b2685899bdc338fabb8bd65e8ce92
IotaHydrae <writeforever@foxmail.com>
2023-02-28 11:15:31 +0800

    add tests
    Signed-off-by: IotaHydrae <writeforever@foxmail.com>

---
+++ tests/sample.c
@@ -0,0 +1,216 @@
+/**
+ * @file usbmouse.c
+ * @author IotaHydrae (writeforever@foxmail.com)
+ * @brief find the usb mouse and read data
+ * @version 0.1
+ * @date 2023-02-28
+ *
+ * MIT License
+ *
+ * Copyright 2022 IotaHydrae(writeforever@foxmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <libusb-1.0/libusb.h>
+
+int endpoint_out;
+int endpoint_in;
+int interface_n;
+
+enum usb_device_result {
+    dr_vidpid_ok,
+    dr_vidpid_unknown,
+    dr_error,
+};
+
+#define VENDOR_ID_RAZER     0x1532
+#define PRODUCT_ID_MOUSE    0x006e
+
+static int usb_mouse_probe(libusb_device *dev, libusb_device_handle **dev_handle)
+{
+    struct libusb_device_descriptor desc;
+    struct libusb_config_descriptor *config;
+
+    *dev_handle = NULL;
+
+    int rc = libusb_get_device_descriptor(dev, &desc);
+    if (rc) {
+        printf("Failed to get device descriptor\n");
+        return rc;
+    }
+
+    rc = libusb_get_active_config_descriptor(dev, &config);
+    if (rc) {
+        printf("Failed to read config descriptor\n");
+        return rc;
+    }
+
+    /* vendor and product id specify */
+    if (desc.idVendor != VENDOR_ID_RAZER) {
+        printf("Unrecognizable vendor 0x%04x!\n", desc.idVendor);
+        return dr_vidpid_unknown;
+    }
+
+    switch (desc.idProduct) {
+    case PRODUCT_ID_MOUSE:
+        printf("This is the USB device we want 0x%04x:0x%04x\n", desc.idVendor, desc.idProduct);
+        break;
+    default:
+        printf("Unrecognizable product! 0x%04x\n", desc.idProduct);
+        return dr_vidpid_unknown;
+    }
+
+    /* interface specify */
+    for (int interface = 0; interface < config->bNumInterfaces; interface++) {
+        const struct libusb_interface_descriptor *intf_desc =
+                        &config->interface[interface].altsetting[0];
+        int interface_num = intf_desc->bInterfaceNumber;
+
+        if (intf_desc->bInterfaceClass != LIBUSB_CLASS_HID
+            || intf_desc->bInterfaceProtocol != 2) {
+            continue;
+        } else {
+            for (int ep = 0; ep < intf_desc->bNumEndpoints; ep++) {
+                if ((intf_desc->endpoint[ep].bmAttributes & 3) != LIBUSB_TRANSFER_TYPE_INTERRUPT ||
+                    (intf_desc->endpoint[ep].bEndpointAddress & 0x80) != LIBUSB_ENDPOINT_IN) {
+                    printf("skipping interface %d, endpoint[%d] is not bulk out", interface_num, ep);
+                    continue;
+                } else {
+                    printf("founded interface %d, endpoint[%d]\n", interface_num, ep);
+                    printf("wMaxPacketSize : %d\n", intf_desc->endpoint[ep].wMaxPacketSize);
+                    endpoint_in = intf_desc->endpoint[ep].bEndpointAddress;
+                    interface_n = interface;
+                    goto founded;
+                }
+            }
+        }
+    }
+
+founded:
+    /* free alloced resource */
+    libusb_free_config_descriptor(config);
+
+    return dr_vidpid_ok;
+}
+
+int main()
+{
+    int rc;
+    int num_devices;
+    int found;
+    libusb_device *dev, **devs;
+    libusb_device_handle *dev_handle;
+
+    rc = libusb_init(NULL);
+    if (rc < 0) {
+        printf("failed to initialize libusb");
+        exit(1);
+    }
+
+    /* get device list */
+    if ((num_devices = libusb_get_device_list(NULL, &devs)) < 0) {
+        printf("libusb get device list failed ! %s \n", libusb_error_name(num_devices));
+        libusb_exit(NULL);
+        exit(1);
+    }
+
+    /* for each device, get config descriptor */
+    for (int i = 0; i < num_devices; i++) {
+        dev = devs[i];
+
+        rc = usb_mouse_probe(dev, &dev_handle);
+        if (rc == dr_vidpid_ok)
+            break;
+
+        struct libusb_config_descriptor *config_desc;
+        rc = libusb_get_config_descriptor(dev, 0, &config_desc);
+        if (rc) {
+            printf("failed to get configuration descriptor\n");
+            continue;
+        }
+
+        for (int interface = 0; interface < config_desc->bNumInterfaces; interface++) {
+            const struct libusb_interface_descriptor *intf_desc =
+                            &config_desc->interface[interface].altsetting[0];
+            int interface_num = intf_desc->bInterfaceNumber;
+
+            if (intf_desc->bInterfaceClass != 255 || intf_desc->bInterfaceProtocol != 0) {
+                continue;
+            } else { /* this is the device that we want */
+                /* parse interface descriptors, find correct endpoint */
+                for (int ep = 0; ep < intf_desc->bNumEndpoints; ep++) {
+                    if ((intf_desc->endpoint[ep].bmAttributes & 3) != LIBUSB_TRANSFER_TYPE_BULK ||
+                        (intf_desc->endpoint[ep].bEndpointAddress & 0x80) != LIBUSB_ENDPOINT_OUT) {
+                        printf("skipping interface %d, endpoint[%d] is not bulk out", interface_num, ep);
+                        continue;
+                    } else {
+                        printf("founded interface %d, endpoint[%d]\n", interface_num, ep);
+                        printf("wMaxPacketSize : %d\n", intf_desc->endpoint[ep].wMaxPacketSize);
+                        endpoint_out = intf_desc->endpoint[ep].bEndpointAddress;
+                        found = 1;
+                        interface_n = interface;
+                        goto dev_founded;
+                    }
+                }
+            }
+        }
+
+
+    }
+
+dev_founded:
+    if (found != 1) {
+        printf("device not founded! exiting ...\n");
+        libusb_exit(NULL);
+        exit(1);
+    }
+
+    rc = libusb_open(dev, &dev_handle);
+    if (rc) {
+        fprintf(stderr, "failed to open device\n");
+        exit(1);
+    }
+
+    /* free device list */
+    libusb_free_device_list(devs, true);
+
+    /* detach origin driver */
+    libusb_set_auto_detach_kernel_driver(dev_handle, true);
+
+    /* clain interface */
+    rc = libusb_claim_interface(dev_handle, interface_n);
+    if (rc) {
+        fprintf(stderr, "failed to claim interface\n");
+        exit(1);
+    }
+
+    printf("device opened! ep addr : 0x%02x\n", endpoint_out);
+    int size;
+    uint8_t buffer[64];
+
+    libusb_release_interface(dev_handle, interface_n);
+    libusb_close(dev_handle);
+    libusb_exit(NULL);
+
+    return 0;
+}
